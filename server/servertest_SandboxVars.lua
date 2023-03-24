6ac26b31c443edbc899288535dcbd6e1ddd6a217
Gameciel <christofer.calvin.pranata@gmail.com>
2023-03-24 20:10:05 +0700

    StarterKit to false

--- server/servertest_SandboxVars.lua
+++ server/servertest_SandboxVars.lua
@@ -234,7 +234,7 @@ SandboxVars = {
     -- 5 = Often
     LockedHouses = 6,
     -- Spawn with chips, water bottle, school bag, baseball bat and a hammer.
-    StarterKit = true,
+    StarterKit = false,
     -- Nutritional value of food affects the player's condition.
     Nutrition = true,
     -- Define how fast the food will spoil inside or outside fridge. Default=Normal
